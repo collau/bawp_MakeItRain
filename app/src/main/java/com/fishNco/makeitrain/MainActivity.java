437bbc17ee7edd08ef9ef517b1f5b990c19d4b5b
junyi <collau@hotmail.com>
2020-10-09 02:04:05 +0800

    Challenge and Solution

--- app/src/main/java/com/fishNco/makeitrain/MainActivity.java
+++ app/src/main/java/com/fishNco/makeitrain/MainActivity.java
@@ -2,6 +2,7 @@ package com.fishNco.makeitrain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -33,6 +34,24 @@ public class MainActivity extends AppCompatActivity {
                 NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
                 moneyCounter += 1000;
                 moneyText.setText(String.valueOf(numberFormat.format(moneyCounter)));
+
+                //CHALLENGE: Change color of text when it hits 10,000
+                if (moneyCounter >= 10000)
+                    moneyText.setTextColor(getResources().getColor(R.color.myColor));
+
+                switch (moneyCounter) {
+                    case 2000:
+                        moneyText.setTextColor(Color.BLACK);
+                        break;
+                    case 4000:
+                        moneyText.setTextColor(Color.RED);
+                        break;
+                    case 6000:
+                        moneyText.setTextColor(Color.GREEN);
+                        break;
+                    default:
+                        break;
+                }
             }
         });
 
